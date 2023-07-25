7ef267380232ee3f5276fc4d4a032467b10c324f
ankurkhade <58468803+ankurkhade@users.noreply.github.com>
2023-07-25 15:19:40 +0530

    Update lastpass.tf

--- lastpass.tf
+++ lastpass.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 data "external" "lastpass_credentials" {
-  program = ["sh", "/home/ankur/Downloads/tanzu/tanzify-username-live/gcp-PA-arau/us-east1/demo1/_scripts/script.sh"]
+  program = jsonencode(["sh", "/home/ankur/Downloads/tanzu/tanzify-username-live/gcp-PA-arau/us-east1/demo1/_scripts/script.sh"])
 }
 
 output "google-service-key" {
