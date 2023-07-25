b218fda9dffd6d5d0b72b831bdb23a5c0c9a322a
ankurkhade <58468803+ankurkhade@users.noreply.github.com>
2023-07-25 15:10:24 +0530

    Create lastpass.tf

---
+++ lastpass.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    lastpass = {
+      source = "nrkno/lastpass"
+      version = "0.5.1"
+    }
+  }
+}
+
+data "external" "lastpass_credentials" {
+  program = ["sh", "/home/ankur/Downloads/tanzu/tanzify-username-live/gcp-PA-arau/us-east1/demo1/_scripts/script.sh"]
+}
+
+output "google-service-key" {
+  value = jsondecode(data.external.lastpass_credentials.result)
+}
